b41b731c60817ae9d7891dc8982582250fa41c54
Jose Lorenzo Faelman <63888971+jlfaelman@users.noreply.github.com>
2025-02-01 11:42:48 +0800

    customer.service: added CRUD operations

--- src/app/services/customers.service.ts
+++ src/app/services/customers.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Customer } from '../interface/customer';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class CustomersService {
 
-  constructor() { }
+  private apiUrl = 'http://127.0.0.1:8000/api/customer';
+
+  constructor(private http: HttpClient) { }
+
+  createUser(customer: Customer): Observable<Customer> {
+    return this.http.post<Customer>(this.apiUrl, customer);
+  }
+
+
+  getUsers(): Observable<Customer[]> {
+    return this.http.get<Customer[]>(this.apiUrl);
+  }
+
+
+  getUserById(id: number): Observable<Customer> {
+    return this.http.get<Customer>(`${this.apiUrl}/${id}`);
+  }
+
+
+  updateUser(id: number, customer: Customer): Observable<Customer> {
+    return this.http.put<Customer>(`${this.apiUrl}/${id}`, customer);
+  }
+
+
+  deleteUser(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
